174b421276f09a6eda08f55886ebb596bdf50328
Duarte Carvalho <up201503661@edu.fe.up.pt>
2022-12-21 13:15:57 +0000

    fixed config

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ module.exports = {
     async headers() {
     return [
       {
-        // matching all API routes
-        source: "/api/:path*",
+        // matching all routes
+        source: "/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
