3bb3b22e02adcdefb15cb702aba783654fcd7341
Duarte Carvalho <duarte.carvalho@g.xarevision.pt>
2022-12-23 10:29:54 +0000

    added icon and meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -14,14 +14,17 @@ function MyApp({ Component, pageProps }) {
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet" />
                 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3931171896983486" crossOrigin="true"></script>
-                {/*
-                 <link rel="icon" type="image/png" href="favicon.png"/>
-                <meta property="og:title" content='Impac&apos;tu' />
-                <meta property="og:url" content='https://www.impactu.org' />
-                <meta property="og:description" content='Somos uma Associação de estudantes do Porto que ajuda pessoas e famílias carenciadas. Vamos criar impacto?'/>
-                <meta property="og:image" content='./favicon.png'/>
-                <link rel="preconnect" href="https://fonts.gstatic.com"/>
-                <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet"/> */}
+                <link rel="icon" type="image/png" href="icon.jpeg" />
+                {/* FACEBOOk */}
+                <meta property="og:title" content='Simular CA' />
+                <meta property="og:url" content='https://ca-simulator.vercel.app/' />
+                <meta property="og:description" content='Simula a rentabilidade do teu aforro!' />
+                <meta property="og:image" content='icon.jpeg' />
+                {/* TWITTER */}
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content="Simular CA" />
+                <meta name="twitter:description" content="Simula a rentabilidade do teu aforro!" />
+                <meta name="twitter:image" content="icon.jpeg" />
             </Head>
             <Component pageProps={pageProps} />
             <Analytics />
